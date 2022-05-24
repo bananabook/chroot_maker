2d6dca56b81d26015969e23a5586afffa3577393
bananabook <leach-david@web.de>
2022-05-24 22:53:33 +0200

    variable instead of repeated call of sed

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,10 +1,12 @@
+wishlist=$(sed -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' wishlist )
+
 cp --parent $(
-	sed -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' wishlist |
+	echo $wishlist|sed 's/ /\n/g'|
 		while read bin;do
 			ldd $(which $bin)|grep -oP '/lib\S*'
 		done |sort|uniq
 ) -t fake_root
-sed -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' wishlist |
+echo $wishlist|sed 's/ /\n/g'|
 	while read bin;do
 		cp --parent $(which $bin|sed 's/\/usr//g') ./fake_root
 	done
